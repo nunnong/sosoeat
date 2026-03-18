9914fad8eab0d63c99e13a6bb6c9f51afb0d3430
sang1 <dign0614@gmail.com>
2026-03-18 13:36:26 +0900

    refactor: 폴더 및 파일 구조 변경

--- src/components/ui/progress-bar/index.ts
+++ src/components/ui/progress-bar/index.ts
@@ -1,3 +1,2 @@
-export { Progress } from '@/components/ui/progress-bar/progress';
-export type { ProgressProps } from '@/components/ui/progress-bar/progress.type';
-export { ProgressWithLabel } from '@/components/ui/progress-bar/progress-with-label';
+export { Progress } from './progress';
+export type { ProgressProps } from './progress.type';
