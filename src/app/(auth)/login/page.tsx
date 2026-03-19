8b38a3a4bf623787304926995e443e73281429f4
sang1 <dign0614@gmail.com>
2026-03-19 14:56:24 +0900

    refactor: 로그인 페이지 및 스토리북에 LoginLayout 적용 및 주석 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,16 @@
 'use client';
 
-import Link from 'next/link';
-
-import { GoogleIcon, KakaoIcon } from './_components/icons';
 import { LoginForm, LoginFormValues } from './_components/login-form';
+import { LoginLayout } from './_components/login-layout/login-layout';
 
 export default function LoginPage() {
+  /**
+   * 사용자가 입력한 데이터를 바탕으로 로그인을 시도합니다.
+   * 현재는 테스트를 위해 지연 시간이 있는 Mock 함수로 구현되어 있으며,
+   * 추후 백엔드 API와의 실제 연동이 필요한 부분입니다.
+   *
+   * @param {LoginFormValues} data - 사용자가 입력한 이메일 및 비밀번호 데이터
+   */
   const handleLogin = async (data: LoginFormValues) => {
     return new Promise<void>((resolve) => {
       setTimeout(() => {
@@ -16,44 +21,8 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="bg-sosoeat-gray-100 flex h-[592px] min-h-screen items-center justify-center px-4">
-      <div className="flex w-full max-w-[568px] flex-col rounded-[24px] bg-white px-4 py-6 shadow-sm transition-all duration-200 ease-in-out sm:rounded-[40px] sm:px-[56px] sm:pt-[48px] sm:pb-[44px]">
-        <h1 className="mb-10 text-center text-2xl font-bold">로그인</h1>
-
-        <LoginForm onSubmit={handleLogin} />
-
-        <div className="relative mt-10 flex items-center justify-center">
-          <div className="border-sosoeat-gray-400 absolute w-full border-t"></div>
-          <span className="text-sosoeat-gray-600 relative bg-white px-4 text-base font-medium">
-            SNS 계정으로 로그인
-          </span>
-        </div>
-
-        <div className="mt-6 flex flex-col items-center gap-[12px] sm:flex-row sm:gap-[16px]">
-          <button
-            type="button"
-            className="flex h-[48px] w-full items-center justify-center gap-[12px] rounded-[12px] border border-gray-300 bg-white text-base font-semibold text-slate-800 hover:bg-gray-50 focus:ring-2 focus:ring-gray-200 focus:ring-offset-1 focus:outline-none sm:flex-1"
-          >
-            <GoogleIcon />
-            구글로 계속하기
-          </button>
-
-          <button
-            type="button"
-            className="flex h-[48px] w-full items-center justify-center gap-[12px] rounded-[12px] bg-[#FFEE01] text-base font-semibold text-slate-800 opacity-90 hover:opacity-100 focus:ring-2 focus:ring-[#FFEE01] focus:ring-offset-1 focus:outline-none sm:flex-1"
-          >
-            <KakaoIcon />
-            카카오로 계속하기
-          </button>
-        </div>
-
-        <p className="text-sosoeat-gray-800 pt-10 text-center text-base font-medium">
-          같이달램이 처음이신가요?{' '}
-          <Link href="/signup" className="text-sosoeat-orange-600 underline underline-offset-4">
-            회원가입
-          </Link>
-        </p>
-      </div>
-    </div>
+    <LoginLayout title="로그인">
+      <LoginForm onSubmit={handleLogin} />
+    </LoginLayout>
   );
 }
