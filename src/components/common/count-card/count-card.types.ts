4120dfb2c7a54dffc6b8fa48a59661e252745c73
naneunyamini <minp4028@gmail.com>
2026-03-25 22:55:28 +0900

    fix: 폴더 삭제

--- src/components/common/count-card/count-card.types.ts
+++
@@ -1,9 +0,0 @@
-import { VariantProps } from 'class-variance-authority';
-
-import { cardVariants } from './count-card.variants';
-
-export interface CountCardProps extends VariantProps<typeof cardVariants> {
-  count: number;
-  title: string;
-  className?: string;
-}
