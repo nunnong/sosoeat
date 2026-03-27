5dce5818ab85af301dd3d7d267b66cde48c587a1
cncn0069 <cncn0069@naver.com>
2026-03-27 18:12:52 +0900

    feat: fetchClient사

--- src/components/common/meeting-create-modal/_components/_components/step2-basic-info/image-submit/image-submit.tsx
+++ src/components/common/meeting-create-modal/_components/_components/step2-basic-info/image-submit/image-submit.tsx
@@ -1,61 +1,52 @@
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 
 import Image from 'next/image';
 
 import { ImagePlus } from 'lucide-react';
 
 import { StepProps } from '@/components/common/meeting-create-modal/meeting-create-modal.types';
+import { fetchClient } from '@/lib/http/fetch-client';
 import { TeamIdImagesPostRequest } from '@/types/generated-client/apis/ImagesApi';
 import { PresignedUrlRequest } from '@/types/generated-client/models/PresignedUrlRequest';
 
 export const ImageSubmit = ({ form }: StepProps) => {
   const { setValue } = form;
   const [uploadedImage, setUploadedImage] = useState<File | null>(null);
-
-  const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEzOTgsInRlYW1JZCI6InNvc29lYXR0ZXN0IiwiZW1haWwiOiJ0ZXN0QGV4YW1wbGUuY29tIiwiaWF0IjoxNzc0NTc1NjQyLCJleHAiOjE3NzQ1NzY1NDJ9.YTxXyp5hwROvGjgzdwMK7a5FsJJh-TTe5g8KYI1A60w`;
+  const [publicUrl, setPublicUrl] = useState<string>('');
 
   const handleUploadImage = async () => {
     if (uploadedImage) {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/${process.env.NEXT_PUBLIC_TEAM_ID}/images`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify({
-            fileName: uploadedImage.name,
-            contentType: uploadedImage.type,
-            folder: 'meetings',
-          } as PresignedUrlRequest),
-        }
-      );
+      const response = await fetchClient.post(`/images`, {
+        fileName: uploadedImage.name,
+        contentType: uploadedImage.type,
+        folder: 'meetings',
+      });
       const data = await response.json();
       if (!response.ok) {
         throw new Error('Failed to upload image');
       }
 
       //사진 업로드
-      await fetch(data.presignedUrl, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': uploadedImage.type,
-        },
-        body: uploadedImage,
-      });
-
+      try {
+        await fetch(data.presignedUrl, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': uploadedImage.type,
+          },
+          body: uploadedImage,
+        });
+      } catch (error) {
+        console.error('Error uploading image:', error);
+        throw new Error('Failed to upload image to S3');
+      }
       //사진 url 저장
       setValue('image', data.publicUrl);
+      setPublicUrl(data.publicUrl);
     }
   };
 
-  useEffect(() => {
-    handleUploadImage();
-  }, [uploadedImage]);
-
-  return form.watch('image') ? (
-    <Image src={form.watch('image')} alt="image" width={147} height={147} className="rounded-2xl" />
+  return publicUrl ? (
+    <Image src={publicUrl} alt="image" width={147} height={147} className="rounded-2xl" />
   ) : (
     <div className="bg-sosoeat-gray-100 text-sosoeat-gray-500 flex h-[147px] w-[147px] flex-col items-center justify-center gap-2 rounded-2xl border border-dashed text-sm">
       <label htmlFor="image" className="text-center text-xs font-medium">
