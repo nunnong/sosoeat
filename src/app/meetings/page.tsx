98011c1fe4d9e25e93f1d362a31b5f01f3d8b7f6
sang1 <dign0614@gmail.com>
2026-04-01 20:58:38 +0900

    fix: page 수정

--- src/app/meetings/page.tsx
+++ src/app/meetings/page.tsx
@@ -46,42 +46,32 @@ export default function MeetingsPage() {
   //sortBy는 dateTime, registrationEnd, participantCount
   return (
     <div className="bg-sosoeat-gray-100">
-      <div className="mx-auto flex max-w-[1140px] flex-col justify-center gap-4 sm:px-4">
-        <MeetingSearchBanner
-          alt="모임 배너"
-          imageUrl="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?w=1600&q=80"
-          titleContent={<span>함께하면</span>}
-          subtitleContent={
-            <>
-              <br />더 맛있어요
-            </>
-          }
-          subtitle={
-            <p>가고 싶었던 맛집, 혼자 가기 아쉬웠죠? 모여요에서 같이 먹을 사람을 찾아보세요.</p>
-          }
-        />
-        {!meetingData || meetingData.length === 0 ? (
-          <EmptyPage />
-        ) : (
-          <div className="flex flex-col gap-4 px-4 sm:px-0">
-            <MeetingFilterBar
-              regionCommitted={regionCommitted}
-              date={date}
-              typeFilter={typeFilter}
-              onTypeFilterChange={handleTypeFilterChange}
-              onDateChange={handleDateChange}
-              onRegionChange={handleRegionChange}
-              onSortChange={handleSortChange}
-              sort={sort}
-            />
+      <div className="mx-auto flex max-w-[1140px] flex-col items-center justify-center gap-4 sm:px-4">
+        <MeetingSearchBanner />
+        <div className="flex flex-col gap-4 px-4 sm:px-0">
+          <MeetingFilterBar
+            sortBy={sortBy}
+            sortOrder={sortOrder}
+            regionCommitted={regionCommitted}
+            dateStart={dateStart}
+            dateEnd={dateEnd}
+            typeFilter={typeFilter}
+            onTypeFilterChange={handleTypeFilterChange}
+            onDateChange={handleDateChange}
+            onRegionChange={handleRegionChange}
+            onSortChange={handleSortChange}
+          />
+          {!meetingData || meetingData.length === 0 ? (
+            <EmptyPage />
+          ) : (
             <div className="grid grid-cols-1 justify-center gap-1 md:grid-cols-2 md:gap-[20px] lg:grid-cols-3 lg:gap-[27px]">
               {meetingData.map((meeting) => (
                 <MainPageCard key={meeting.id} meeting={meeting} />
               ))}
             </div>
-          </div>
-        )}
-        <MeetingMakeButton onClick={handleOpenCreateModal} />
+          )}
+          <MeetingMakeButton onClick={handleOpenCreateModal} />
+        </div>
         <MeetingCreateModal open={isOpen} onClose={close} onSubmit={createMeeting} />
       </div>
     </div>
